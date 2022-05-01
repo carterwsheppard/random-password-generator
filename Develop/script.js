7666f4cb0bcd6df56985f3fae73f3e3d36a2254b
carterwsheppard <carterwsheppard@gmail.com>
2022-05-01 11:09:28 -0400

    added window prompts for when I press the generate button. Limited responses to integers and created prompt to ask to try again if integers are not entered.

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,37 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+ // passwordText.value = password;
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var characters 
+
+
+var criteria = function() {
+
+  var characters = window.prompt("How many characters do you want? Please enter a number 1 - 10.");
+
+  var numbers = window.prompt("How many numbers do you want? Please enter a number 1 - 10.");
+
+  var specialcharacters = window.prompt("How many special characters do you want? Please enter a number 1 - 10.");
+
+  if ((Number.isInteger(parseInt(characters))) === false || (Number.isInteger(parseInt(numbers))) === false || (Number.isInteger(parseInt(specialcharacters))) === false) {
+  window.alert("At least one of the values you entered is not a number. Please try again.");
+  criteria();
+ }
+  //else { writePassword() }
+}
+
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", criteria);
+
+
+
