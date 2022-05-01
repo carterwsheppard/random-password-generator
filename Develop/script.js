714353e3ed0a62166f07a56d8926123aa54d3c1e
carterwsheppard <carterwsheppard@gmail.com>
2022-05-01 11:54:19 -0400

    now checks the number of each character type on confirm step

--- Develop/script.js
+++ Develop/script.js
@@ -5,32 +5,33 @@
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-}
+  //passwordText.value = password;
+//}
 
 // Add event listener to generate button
-var characters 
 
 
 var criteria = function() {
+
+  window.alert("This password generator has the following requirements ~ Your password must have between 8 and 128 characters total.");
 
   characters = null;
   numbers = null;
   specialcharacters = null;  
 
-  var characters = window.prompt("How many characters do you want in your password? Please enter a number 1 - 10.");
+  var characters = window.prompt("How many alphabetic characters do you want in your password? Please enter a number 0 - 128.");
 
   if (characters === null) {return}
 
-  var numbers = window.prompt("How many numbers do you want in your password? Please enter a number 1 - 10.");
+  var numbers = window.prompt("How many numbers do you want in your password? Please enter a number 0 - 128.");
 
   //if (characters === null) {return}
 
-  var specialcharacters = window.prompt("How many special characters do you want in your password? Please enter a number 1 - 10.");
+  var specialcharacters = window.prompt("How many special characters do you want in your password? Please enter a number 0 - 128.");
 
   //if (characters === null) {return}
 
@@ -40,11 +41,15 @@ var criteria = function() {
  }
 
  var passlength = parseInt(characters) + parseInt(numbers) + parseInt(specialcharacters);
- var confirmLength = window.confirm("To confirm, you selected a total of " + passlength + " characters for your password. Is this correct? If not, you can re-enter the number of each character type you'd like.");
+ if ( 8 > passlength || passlength > 128) {
+   window.prompt("Sorry - your selections did not meet the criteria for having 8 - 128 total characters. Please try again.");
+   criteria();
+ }
 
- console.log(confirmLength)
+
+ var confirmLength = window.confirm("To confirm, you selected a total of " + passlength + " characters for your password. " + parseInt(characters) + " alphabetic characters will be used, " + parseInt(numbers) + " numeric characters will be used, and " + parseInt(specialcharacters) + " special characters will be used. Is this correct? If not, you can re-enter the number of each character type you'd like.");
+ 
  if (confirmLength === false) {criteria()};
-  else { writePassword() }
 }
 
 var generateBtn = document.querySelector("#generate");
